65f1331db7d6f4f0237c8cac856b866885114292
hoseajerardllagas <hoseajerard.llagas@benilde.edu.ph>
2025-10-08 15:11:04 +0800

    Added a sample change comment

--- Sample Project/src/Main.java
+++ Sample Project/src/Main.java
@@ -1,10 +1,25 @@
-
+import java.util.Scanner;
 
 public  class Main {
 
         public static void main(String[] args) {
 
             // prints out hello world
-            System.out.println("Hello world");
+            //System.out.println("Hello world");
+
+            // gets user input
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("What is your name");
+            String name = scanner.nextLine();
+
+            System.out.println("Hello " + name);
+
+            //This is a change!!!!!
+
+
+
+
+
         }
 }
